75e983b3e0a8478adef7bb4016f1efdfb55164cc
David Parnell <dreply@yahoo.com>
2011-07-09 07:09:25 -0700

    Added layouts and integration test

--- Gemfile
+++ Gemfile
@@ -6,10 +6,11 @@ gem 'sqlite3-ruby', '1.2.5', :require => 'sqlite3'
 
 
 group :development, :test do
-   gem 'rspec-rails', '2.6.1'
+  gem 'rspec-rails', '2.6.1'
 end
 
 group :test do
-   gem 'rspec', '2.6.0'
-   gem 'webrat'
+  gem 'rspec', '2.6.0'
+  gem 'webrat'
+  gem 'spork', '0.8.5'
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -39,7 +39,6 @@ describe PagesController do
     end
   end
 
-
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
@@ -50,6 +49,19 @@ describe PagesController do
       get 'about'
       response.should have_selector("title",
                                     :content => "#{@base_title} | About")
+    end
+  end
+
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+                                    :content => "#{@base_title} | Help")
     end
   end
 
